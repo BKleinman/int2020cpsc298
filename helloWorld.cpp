a12bbf7ae4fee7200b1cb1282772f260550f9753
BKleinman <kleinman@chapman.edu>
2020-01-14 20:53:37 +0000

    i made additional changes

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -3,5 +3,7 @@ using namespace std;
 
 int main(){
   cout << "Hello World." << endl;
+  cout << "I had to add this statement because rene said so" << endl;
+  cout << "Adding another cout" << endl;
   return 0;
 }
